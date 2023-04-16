367502e074ffae7d4379f7e55c1d737af27c3b4a
Zhiqing-Xu <palladium.xu@gmail.com>
2023-04-15 20:25:51 -0400

    u

--- pages/script.js
+++ pages/script.js
@@ -14,16 +14,26 @@ let previous = document.getElementById('previous');
 
 
 let songs = [
-    { songName: "Warriyo", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "She Move It Like", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
-    { songName: "Oxygen", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
-    { songName: "Mia", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
-    { songName: "Wo Kehte Hai", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
-    { songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg" },
-    { songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg" },
-    { songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg" },
-    { songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg" },
-    { songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg" },
+    { songName: "Warriyo"                        , filePath: "songs/1.mp3", coverPath: "covers/1.jpg"   },
+    { songName: "She Move It Like"               , filePath: "songs/2.mp3", coverPath: "covers/2.jpg"   },
+    { songName: "Oxygen"                         , filePath: "songs/3.mp3", coverPath: "covers/3.jpg"   },
+    { songName: "Mia"                            , filePath: "songs/4.mp3", coverPath: "covers/4.jpg"   },
+    { songName: "Wo Kehte Hai"                   , filePath: "songs/5.mp3", coverPath: "covers/5.jpg"   },
+    { songName: "Rabba - Salam-e-Ishq"           , filePath: "songs/2.mp3", coverPath: "covers/6.jpg"   },
+    { songName: "Sakhiyaan - Salam-e-Ishq"       , filePath: "songs/2.mp3", coverPath: "covers/7.jpg"   },
+    { songName: "Bhula Dena - Salam-e-Ishq"      , filePath: "songs/2.mp3", coverPath: "covers/8.jpg"   },
+    { songName: "Tumhari Kasam - Salam-e-Ishq"   , filePath: "songs/2.mp3", coverPath: "covers/9.jpg"   },
+    { songName: "Na Jaana - Salam-e-Ishq"        , filePath: "songs/4.mp3", coverPath: "covers/1.jpg"   },
+    { songName: "Rabba - Salam-e-Ishq"           , filePath: "songs/2.mp3", coverPath: "covers/6.jpg"   },
+    { songName: "Sakhiyaan - Salam-e-Ishq"       , filePath: "songs/2.mp3", coverPath: "covers/7.jpg"   },
+    { songName: "Bhula Dena - Salam-e-Ishq"      , filePath: "songs/2.mp3", coverPath: "covers/8.jpg"   },
+    { songName: "Tumhari Kasam - Salam-e-Ishq"   , filePath: "songs/2.mp3", coverPath: "covers/9.jpg"   },
+    { songName: "Na Jaana - Salam-e-Ishq"        , filePath: "songs/4.mp3", coverPath: "covers/1.jpg"   },
+    { songName: "Rabba - Salam-e-Ishq"           , filePath: "songs/2.mp3", coverPath: "covers/6.jpg"   },
+    { songName: "Sakhiyaan - Salam-e-Ishq"       , filePath: "songs/2.mp3", coverPath: "covers/7.jpg"   },
+    { songName: "Bhula Dena - Salam-e-Ishq"      , filePath: "songs/2.mp3", coverPath: "covers/8.jpg"   },
+    { songName: "Tumhari Kasam - Salam-e-Ishq"   , filePath: "songs/2.mp3", coverPath: "covers/9.jpg"   },
+    { songName: "Na Jaana - Salam-e-Ishq"        , filePath: "songs/4.mp3", coverPath: "covers/1.jpg"   },
 ]
 
 
