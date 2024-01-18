b61798fbce2ef9e1a978578c4cdf0e1ae12272eb
Zhiqing-Xu <palladium.xu@gmail.com>
2024-01-18 04:27:43 -0500

    u

--- pages/music.js
+++
@@ -1,7 +0,0 @@
-bubbly({
-    colorStart: '#fff4e6',
-    colorStop: '#ffe9e4',
-    blur: 1,
-    compose: 'source-over',
-    bubbleFunc:() => `hsla(${Math.random() * 9999}, 100%, 50%, .3)`
-  });
